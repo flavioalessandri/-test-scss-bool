6c17e87342af9ee468d8b82274dd828daf10b9d9
andreacapntd <andreacapntd@gmail.com>
2020-10-30 20:17:51 +0100

    add sponsorship & checkout view

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main-layout')
+
+@section('header')
+  @include('partials.header2')
+@endsection
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Riepilogo operazione</div>
+
+                <div class="card-body">
+
+                  <h1>Transazione avvenuta con successo</h1>
+                  <p>Appartamento selezionato: {{ $apartment -> description }}</p>
+                  <p>Tipologia di sponsorizzazione: {{ $sponsorship -> type_of_sponsorship }} - {{ $sponsorship  -> price }}€ / {{ $sponsorship  -> deadline }}h</p>
+                  <p>Il numero transazione è: {{ $transaction_id }}</p>
+
+                  <a class="btn btn-primary" href="{{route('user.index')}}">I tuoi annunci</a>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
