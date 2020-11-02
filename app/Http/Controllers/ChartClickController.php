2108709ef00aa827693dc9cfda4b9d34b97c41fb
Flavio <flavio.alessandri@gmail.com>
2020-11-02 10:02:11 +0100

    ChartClickController

--- app/Http/Controllers/ChartClickController.php
+++ app/Http/Controllers/ChartClickController.php
@@ -21,7 +21,6 @@ class ChartClickController extends Controller
   public function statistic( Request $request )
     {
          $data = $request['id'];
-         // passing data to another method in the same controller
          $this->statisticJson( $request);
          return view('statistic', compact('data'));
     }
@@ -32,36 +31,6 @@ class ChartClickController extends Controller
       $id = $request['id'];
       $statistic = Statistic::where('apartment_id',"=", $id)
         ->get();
-
-      //   $array = array();
-      //
-      //   for ($i=0; $i < 12 ; $i++) {
-      //
-      //     $arr = array(
-      //       'current_date' => "",
-      //       'number_of_click'=> 0
-      //               );
-      //     array_push($array,$arr);
-      //   };
-      //
-      //
-      //
-      // $cont = count($statistic);
-      //
-      // for ($i=0; $i < $cont ; $i++) {
-      //
-      //   if (  (Carbon::parse($statistic[$i]['current_date']) ->format('m') == strval($i) ) {
-      //
-      //     $newArr = array(
-      //       'current_date' => $statistic[$i]['current_date'],
-      //       'number_of_click'=> $statistic[$i]['number_of_click'],
-      //           );
-      // 
-      //     array_replace($array, $newArr);
-      //     }
-      // }
-      //
-      // dd($array, $statistic,$cont);
       return response()->json($statistic);
     }
 
