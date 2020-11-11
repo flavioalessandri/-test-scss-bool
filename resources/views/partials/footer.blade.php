93cb24617b4911e39b03c4df876b0298bd6a3943
Flavio <flavio.alessandri@gmail.com>
2020-11-11 17:54:07 +0100

    updated footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,190 @@
-<footer>
-  {{-- footer --}}
+<!-- Footer -->
+
+<!-- <div class="container fluid"> -->
+
+<footer class=" mt-5 page-footer font-small mdb-color pt-4 text-secondary">
+
+  <!-- Footer Links -->
+  <div class="container max-width-1200 text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-5 offset-md-1 col-lg-4 col-xl-4 ml-0 mt-3">
+         <img class="footer-logo" src="/img/pinklogo.png" alt="">  
+        <h6 class="text-uppercase font-weight-bold d-inline ml-2">Boolbnb</h6>
+        <p class="mt-3">Cerca un appartamento, filtra il tuo risultato in base alla distanza dall'indirizzo digitato e ai servizi offerti. Se registrato potrai creare, aggiornare, cancellare i tuoi appartamenti, sponsorizzarli per un dato periodo e accedere alle statistiche degli stessi</p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+
+
+
+      <!-- <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+        <p>
+          <a href="#!">MDBootstrap</a>
+        </p>
+        <p>
+          <a href="#!">MDWordPress</a>
+        </p>
+        <p>
+          <a href="#!">BrandFlow</a>
+        </p>
+        <p>
+          <a href="#!">Bootstrap Angular</a>
+        </p>
+      </div>
+
+      <hr class="w-100 clearfix d-md-none"> -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class=" text-center text-uppercase mb-4 font-weight-bold">Useful links</h6>
+        <ul class="links">
+
+          <li>
+          <a class="text-secondary" href="https://laravel.com">
+          <i class="fab fa-laravel"></i>
+             Laravel
+          </a>
+          </li>
+
+          <li>
+            <a class="text-secondary" href="">
+            <i class="fas fa-money-check-alt"></i>
+              Braintree
+            </a>
+          </li>
+
+          <li>
+              <a class="text-secondary" href="https://www.algolia.com">
+              <i class="fab fa-algolia"></i>               
+                Algolia
+            </a>
+          </li>
+
+          <li>
+            <a class="text-secondary" href="https://www.chartjs.org">
+            <i class="fas fa-chart-bar"></i>  
+                Chart Js
+            </a>
+         </li>
+
+         <li>
+          <a class="text-secondary" href="https://developers.google.com/maps/documentation/javascript/overview">           
+              <i class="fas fa-map-marker-alt"></i>
+                 Google map
+          </a>
+         </li>
+
+        </ul>        
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="text-sm-center text-md-right col-md-4 col-lg-3 col-xl-3 mr-0 mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+
+        <table class="d-inline">    
+          <tbody>
+            <tr>
+              <th scope="row "><i class="fab fa-github pr-2"></i></th>              
+              <td class="border-top-0" >
+                 <a class="text-secondary" href="">Caterina Marian</a>
+              </td>
+            </tr>
+            
+            <tr>
+              <th scope="row"><i class="fab fa-github pr-2"></i></th>
+              <td class="border-top-0">
+                <a class="text-secondary" href="">Davit Janashia</a>           
+              </td>
+            </tr>
+
+            <tr>
+              <th scope="row"><i class="fab fa-github pr-2"></i></th>
+              <td class="border-top-0">
+                <a class="text-secondary" href="">Flavio Alessandri</a>
+              </td>                
+            </tr>
+
+            <tr>
+              <th scope="row"><i class="fab fa-github pr-2"></i></th>
+              <td class="border-top-0">
+                <a class="text-secondary" href="">Andrea Caprara</a>
+              </td>
+            </tr>
+
+            <tr>
+              <th scope="row"><i class="fab fa-github pr-2"></i></th>
+              <td class="border-top-0">
+                <a class="text-secondary" href="">Alfonso Ferrara</a>                
+              </td>
+            </tr>
+          </tbody>
+        </table>
+    
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">Powered By:
+          <a href="https://mdbootstrap.com/">
+            <strong> BooleanCareers</strong>
+          </a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-google-plus-g"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
 </footer>
